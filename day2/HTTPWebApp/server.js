e5334aae8653f133ef776a38c6f9c5ec3b0a2756
Rohit <rohitkawade021@gmail.com>
2021-07-22 01:22:36 +0530

    code to send JSON file as response to browsers request

--- day2/HTTPWebApp/server.js
+++ day2/HTTPWebApp/server.js
@@ -3,14 +3,28 @@
 var http=require("http"); //using http inbuilt module,storing ref to http variable
 //when server will give request we need to give response so we need to handle the request and send back the browser
 //so we will create a handler function and pass its name as argument to createserver function
+var friends={
+
+    'FirstName':'Jaggya',
+    'LastName':'Patil',
+    'email':'jagdishpatil123@gm.com',
+    'contact':'98229073'
+} ;   
 var onRequestHandler=function(request,response)
 {
+   
+    console.log("Request is received---"); //when browser will hit the url then this will be displayed displayed
    //sending a text response to browser:
     //response.write("hey friend, iam Server:ROHIT here...\n"); //we will get this at browser as response
 
     //sending a html response to browser:
-    response.write("<h1>responding in html</h1>");
-   console.log("Request is received.");
+    //response.write("<h1>responding in html</h1>");
+
+    //sending a json object as response to browser:
+     response.writeHead(200,{'Content-Type':"text/json"});
+     response.write(JSON.stringify(friends));
+   
+
     response.end(); //once response is written then end this package by using end();
 };
 
