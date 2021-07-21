ba81dbb09328ff90fb7a4f180e54f36161c19e03
Rohit <rohitkawade021@gmail.com>
2021-07-21 19:53:43 +0530

    added two event listeners:sendEmail and sendSms, which will be automatically called when underBalance event is raised

--- day2/practiceApp/handler.js
+++ day2/practiceApp/handler.js
@@ -3,6 +3,7 @@
 //writing event handling in a seperate file
 //handler file that will contain handlers for the event that will happen in account file
 //exports is used to make these functions available to outside files
+//publishing functions outside thsi file using exports keyword
 
 exports.payIncometax=function()
 {
@@ -12,4 +13,15 @@ exports.payIncometax=function()
 exports.blockAccount=function()
 {
     console.log("Blocking Account Temporarilyyy !!!");
+
+}
+
+exports.sendEmail=function()
+{
+    console.log("Email is sent to your Reg. email id.");
+}
+
+exports.sendSms=function()
+{
+    console.log("SMS is sent to yr Reg. mobile No.");
 }
