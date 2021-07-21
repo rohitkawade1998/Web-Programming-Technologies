7a0e00cd3fd10763fbd77a85dff020dab111ca81
Rohit <rohitkawade021@gmail.com>
2021-07-21 12:26:50 +0530

    modified pract file with new function show2

--- day2/practiceApp/pract.js
+++ day2/practiceApp/pract.js
@@ -1,26 +1,28 @@
 
 //two ways of declaring a functoin in java script:
 
-/* //type1: 
-function show(count)
+ //type1: 
+function show1(count1)
 {
 
-    console.log("inside show");
- count++;
- console.log(count);
+    console.log("inside show1");
+ count1++;
+ console.log(count1);
 
 }
-*/
+
 
 //type2: 
-var show=function(count)
+var show2=function(count2)
 {
 
-    console.log("inside new show");
- count++;
- console.log(count);
+    console.log("inside new show2");
+ count2++;
+ console.log(count2);
 
 }
 
 var num=15;
-show(num);
+show1(num);
+show2(num);
+///we can see they both give same output
