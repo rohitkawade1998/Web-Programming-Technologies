68e72e7bb09acab77f827dce4ef08e4edca2ade1
Rohit <rohitkawade021@gmail.com>
2021-07-22 14:04:30 +0530

    added a simple error handling block inside callback function(onFileRead)

--- day3/FileIOApp/test.js
+++ day3/FileIOApp/test.js
@@ -1,4 +1,5 @@
 //javascript for reading data from a different file
+//simple example for reading friends file from folder data and showing its content
 //showing its contents at the output
 //to perform file I/O operation we need fs module
 //so we will import the fs module
@@ -10,9 +11,18 @@ var filePath="./data/friends.txt";
 
 //defining callback function automatically call when the file is successfully read
 var onFileRead=function(err,data){
+
+    //if there is some error in finding the file then display such message
+    //handling error
+    if(err)
+    {
+        console.log("something went wrong...!!!");
+    }
+    else{
      console.log("reading data from file----");
      console.log(data.toString());//showing contents on output
      //converting data to string using toString function
+    }
 
 };
 
