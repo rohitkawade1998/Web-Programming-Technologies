b569cfcad0169a267e8b1e93515db9ceccd1faa7
Rohit <rohitkawade021@gmail.com>
2021-07-24 13:04:40 +0530

    modified server.js such that send index.html file for default request,and when request is /hello then send a json object

--- day4/ExpressApp/server.js
+++ day4/ExpressApp/server.js
@@ -1,5 +1,9 @@
+//node js web application to send image as response to a browsers request.
+
 var express=require('express'); //importing express module
 var path=require('path');   //importing path module
+
+//console.log("dirname:1"+__dirname); //just to see what path is under __dirname
 
 var app=express();  ///set global object from framework
 // global object app will contain HTTP server
@@ -11,10 +15,23 @@ app.use(staticFolder);
 //express framework setting handlers
 //mapping incoming request to call back function
 app.get("/",(request,response)=>{
-    response.sendFile(__dirname+"/index.html");
-    console.log("success in showing logo"); //after success showing the logo display this on server side
-})
+   
+  response.sendFile(staticFolder+"/index.html");
+     
+});
+app.get("/hello",(request,response)=>{
+   
+   var friends={
+       "name":"ankya",
+       "contact":897555
+
+   };
+  response.send(friends);
+     
+});
 
 app.listen(9000); //application will listen on 9000 port number
 //showing the message on serverside that server is listening on porn number 9000
+
+
 console.log("Express Application listening on port number: 9000");
