527f4469f5447194bdde1c9733ce8dcba52b6848
Rohit <rohitkawade021@gmail.com>
2021-07-24 15:51:56 +0530

    web application for two web pages, when default(/) request is sent by browser home page is displayed(index.html file is sent),and naivigation tool is available at the home page to go to the about page(about.html file is sent),both the pages are having navigation tool to navigate to another page

--- day4/ExpressApp/server.js
+++ day4/ExpressApp/server.js
@@ -7,28 +7,34 @@ var path=require('path');   //importing path module
 
 var app=express();  ///set global object from framework
 // global object app will contain HTTP server
+
+// __dirname ---->  physical path : F:\webtech\day4\ExpressApp
+               //   virtual path:  http://localhost:9000
+
 var staticFolder=express.static(path.join(__dirname,"public"));
 
 //express framework configuration
-app.use(staticFolder);
+app.use(staticFolder); //configure static middleware
 
 //express framework setting handlers
 //mapping incoming request to call back function
+//two request handlers are implemented
+//1st:request handler for url:http://localhost:9000
 app.get("/",(request,response)=>{
    
   response.sendFile(staticFolder+"/index.html");
      
 });
+//2nd:request handler for url: http://localhost:9000/hello
 app.get("/hello",(request,response)=>{
-   
-   var friends={
-       "name":"ankya",
-       "contact":897555
-
-   };
-  response.send(friends);
+   var friends={ "name":"ankya", "contact":897555};
+   response.send(friends);
      
 });
+
+
+//both of these handlers are HTTP request handlers of type GET Request
+//GET function takes two parameters:(path,callback function)
 
 app.listen(9000); //application will listen on 9000 port number
 //showing the message on serverside that server is listening on porn number 9000
