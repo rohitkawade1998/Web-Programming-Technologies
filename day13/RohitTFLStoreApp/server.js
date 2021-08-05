4430f057f3983fef75940e8067e79b3bdd7ee295
Rohit <rohitkawade021@gmail.com>
2021-08-06 01:23:39 +0530

    Written server side code inside server.js file, imported express,path module, declared public folder as static resource holder, written a GET rest api for default browsers request, added listeners

--- day13/RohitTFLStoreApp/server.js
+++ day13/RohitTFLStoreApp/server.js
@@ -1,3 +1,25 @@
 //this is Flower Store Application
 //developing using : SOC--Seperation Of Concern
 
+//importing the express,path modules:
+
+const express=require('express');
+var path=require('path');
+
+//creating object for global function
+var app=express();
+
+//configure Express Middleware:
+//HTTP middleware configuration:
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//get type function for default browsers request:
+app.get("/",function(req,res){
+    res.sendFile("index.html");
+});
+
+//listen Mode
+app.listen(9898);
+console.log("Rohit: Express TFLSTORE APP i listening on Port Number: 9800");
