5c3e7d9d8297e9707fc8c65aaeba9cd5553b80aa
Rohit <rohitkawade021@gmail.com>
2021-08-06 01:32:44 +0530

    created router.js file which will accept request form server,for any type of request(GET,PUT,POST and DELETE),router maps them with the proper callback function(inside controller)

---
+++ day13/RohitTFLStoreApp/router.js
@@ -0,0 +1,6 @@
+
+//this file will accept request form server
+//for any type of request(GET,PUT,POST and DELETE) 
+// router maps them with the proper callback function(inside controller)
+//its only work is to join the request with the respective Handler function
+
